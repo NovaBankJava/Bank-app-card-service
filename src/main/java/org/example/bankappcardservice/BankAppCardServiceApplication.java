079155ee33610941de8bc0d5bd1361575d3aa849
G. Sales <gustavosales0809@gmail.com>
2026-09-10 14:05:16 -0300

    first commit

---
+++ src/main/java/org/example/bankappcardservice/BankAppCardServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankappcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppCardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAppCardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bankappcardservice/BankAppCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankappcardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppCardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
